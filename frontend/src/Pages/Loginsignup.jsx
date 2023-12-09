0866144b77a53feee97873a0d30731831ede178a
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2023-12-09 17:43:26 +0530

    code uploded

---
+++ frontend/src/Pages/Loginsignup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loginsignup = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  )
+}
+
+export default Loginsignup
---
+++ frontend/src/Pages/ShopCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShopCategory = () => {
+  return (
+    <div>
+      <h1>Shop Category</h1>
+    </div>
+  )
+}
+
+export default ShopCategory
